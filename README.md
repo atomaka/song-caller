fcf0ef7c58d0265139da09d8ac15c85fd9da20db
Andrew Tomaka <atomaka@gmail.com>
2013-02-26 17:27:12 -0500

    Add Scheduling Capabilities
    Implement a scheduler based on crontab to execute calls at specific
    times.  Using PHP-Cron, we can implement a single cron at the system
    level and then use methods to add cron jobs at the PHP level.  This
    allows eventually expansion to handling multiple song calls.

---
+++ README.md
@@ -0,0 +1,6 @@
+install
+
+add cronjob to run master file
+```
+* * * * * cd /path/to/project && php caller.php 1>> /dev/null 2>&1
+```
--- caller.php
+++ caller.php
@@ -3,20 +3,26 @@ require 'vendor/autoload.php';
 require 'vendor/twilio/sdk/Services/Twilio.php';
 
 use Symfony\Component\Yaml\Yaml;
+$crontab = new \HybridLogic\Cron\Crontab;
 
 $settings = Yaml::parse('conf/settings.yaml');
-
 $client = new Services_Twilio(
   $settings['twilio']['sid'],
   $settings['twilio']['token']
 );
 
-try {
-  $call = $client->account->calls->create(
-    $settings['application']['from'],
-    $settings['application']['to'],
-    $settings['application']['xml']
-  );
-} catch(Exception $e) {
-  echo 'Error: ' . $e->getMessage();
-}
+
+
+$crontab->add_job(
+  \HybridLogic\Cron\Job::factory('test')
+    ->on('0 10-16 * * *')
+    ->trigger(function() use($settings, $client) {
+      $call = $client->account->calls->create(
+        $settings['application']['from'],
+        $settings['application']['to'],
+        $settings['application']['xml']
+      );
+    })
+);
+
+$crontab->run();
